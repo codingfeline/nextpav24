ad4ec9122a4ba2c6f0156d388a80f6f43d29417e
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-07 17:54:32 +0000

    update legend for spiciness

--- app/components/spiciness.tsx
+++ app/components/spiciness.tsx
@@ -1,7 +1,22 @@
+// import parse from 'html-react-parser'
+
+import { GiChiliPepper } from 'react-icons/gi'
+import reactStringReplace from 'react-string-replace'
+
 const Spiciness = () => {
+  const levels = ['* Mild', '** Medium', '*** Hot']
+
   return (
-    <div className="text-red-600 place-self-center w-full bg-black flex_center p-2">
-      * Mild, ** Medium, *** Hot
+    <div className="text-red-600  w-full p-2 gap-7 flex bg-black flex_center">
+      {/* * Mild, ** Medium, *** Hot */}
+      {levels.map((level, i) => {
+        const parsed = reactStringReplace(level, '*', () => <GiChiliPepper />)
+        return (
+          <span className=" flex " key={i}>
+            {parsed}
+          </span>
+        )
+      })}
     </div>
   )
 }
