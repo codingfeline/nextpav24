336659192812551cbd4bad6ff0e0762b58bb0e89
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-08 19:17:19 +0000

    cosmetics

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <>
       <div className=" opacity-90 flex flex-col w-full">
         <div className="my-3 bg-[#f9e4b7] border-[#caa75c] border p-2 sm:rounded-lg w-full sm:w-6/7">
-          <div className="flex flex-col justify-between gap-6 p-4 text-[#64480a]">
+          <div className="flex flex-col justify-between gap-6 p-4 text-[#64480a] tracking-wider">
             <h1>Authentic Thai Cuisine</h1>
             {/* <Text size="5">
               Thailand, the land of smiles and its unique cuisine is the Buddhist land in
@@ -17,16 +17,19 @@ export default async function Home() {
               renamed Thailand since 1939 in the era of King Rama VIII. Thailand means the
               land of freedom.
             </Text> */}
-            <Flex gap="1">
-              <Text as="p" size="5">
+            <Flex gap="1" className="flex flex-col gap-3 text-xl tracing-wide">
+              <Text as="p">
                 Welcome to Bangkok Pavilion where the vibrant flavours of Thailand come to
                 life. Immerse yourself in the rich culinary traditions of Thai cuisine,
                 crafted with fresh ingredients, bold spices, and authentic recipes passed
-                down through generations. From the comforting warmth of classic Pad Thai
-                to the aromatic allure of Green Curry, every dish is a celebration of
-                Thailand’s diverse and dynamic culture. Join us in the heart of Deal, Kent
-                for a dining experience that’s as welcoming as it is delicious. Let us
-                take your taste buds on a journey through the Land of Smiles!
+                down through generations.
+              </Text>
+              <Text as="p">
+                From the comforting warmth of classic Pad Thai to the aromatic allure of
+                Green Curry, every dish is a celebration of Thailand’s diverse and dynamic
+                culture. Join us in the heart of Deal, Kent for a dining experience that’s
+                as welcoming as it is delicious. Let us take your taste buds on a journey
+                through the Land of Smiles!
               </Text>
             </Flex>
             <section className="relative flex justify-between flex-col sm:flex-row items-start gap-2">
