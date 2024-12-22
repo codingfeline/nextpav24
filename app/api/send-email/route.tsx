bc03986d7d129b019f210718c40a67f9f1890f63
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-22 06:49:54 +0000

    remove name from email

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -6,14 +6,14 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest) {
   const body = await req.json()
-  const { name, email, message } = body
+  const { email, message } = body
   await resend.emails.send({
     from: 'info@bangkokpavilion.co.uk',
     to: 'info@bangkokpavilion.co.uk',
     cc: email,
     bcc: 'eizan.dirahs@gmail.com',
     subject: 'Web enquiry',
-    react: <WelcomeTemplate name={name} message={message} />,
+    react: <WelcomeTemplate message={message} />,
   })
 
   return NextResponse.json({})
--- app/emails/WelcomeTemplate.tsx
+++ app/emails/WelcomeTemplate.tsx
@@ -1,17 +1,15 @@
 import { Body, Container, Html, Preview, Text } from '@react-email/components'
 
 interface Prop {
-  name: string
   message: string
 }
 
-const WelcomeTemplate = ({ name, message }: Prop) => {
+const WelcomeTemplate = ({ message }: Prop) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
       <Body>
         <Container>
-          <Text>Hello {name}</Text>
           <Text>{message}</Text>
         </Container>
       </Body>
