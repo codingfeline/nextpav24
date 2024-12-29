c803404261a2cf70f07cc04d5d376aec04315ea3
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-29 22:57:06 +0000

    display messages using reactmarkdown

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,5 +1,5 @@
 import prisma from '@/prisma/client'
-import { Card } from '@radix-ui/themes'
+import { Card, Flex, Text } from '@radix-ui/themes'
 import ReactMarkDown from 'react-markdown'
 
 const Message = async () => {
@@ -11,8 +11,17 @@ const Message = async () => {
       {messages.map(m => {
         return (
           <div key={m.id}>
-            <Card className="prose messages">
-              <ReactMarkDown>{m.message}</ReactMarkDown>
+            <Card className="prose messages mb-2">
+              <Flex
+                justify="between"
+                className="font-mono border border-b-2 border-gray-400 border-t-0 border-x-0"
+              >
+                <Text>
+                  From {m.name} [{m.email}]
+                </Text>
+                <Text>{m.createdAt.toLocaleString()}</Text>
+              </Flex>
+              <ReactMarkDown className="font-['Verdana']">{m.message}</ReactMarkDown>
             </Card>
           </div>
         )
