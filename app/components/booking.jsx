aaf44c42cff83b178dcb0c0ed03b49dbc4c92f0e
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-02-08 14:24:52 +0000

    fly-in to Bookings

--- app/components/booking.jsx
+++ app/components/booking.jsx
@@ -1,12 +1,17 @@
+'use client'
+import Reveal from './Reveal'
+
 const Booking = () => {
   return (
-    <div className="w-full sm:w-[500px] place-self-center m-3 ">
-      <iframe
-        src="https://svtables.com/widget/?rest_id=403"
-        className="w-full sm:rounded-xl mt-2"
-        height="680px"
-      ></iframe>
-    </div>
+    <Reveal delay="delay-1000">
+      <div className="w-full sm:w-[500px] place-self-center m-3 ">
+        <iframe
+          src="https://svtables.com/widget/?rest_id=403"
+          className="w-full sm:rounded-xl mt-2"
+          height="680px"
+        ></iframe>
+      </div>
+    </Reveal>
   )
 }
 
