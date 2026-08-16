3e397157895db09fd56c3006b65c17f2f6c87c41
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-08-16 16:44:51 +0100

    move wine prices

--- app/components/table_3_col.js
+++ app/components/table_3_col.js
@@ -21,12 +21,28 @@ const Table3_col = ({ menus, title }) => {
             return (
               <tr key={index}>
                 <td className="row_number">{menu.item_id}</td>
-                <td>{menu.item}</td>
+                <td>
+                  {menu.item}
+                  <span className="winePrices md:!hidden">
+                    {menu.price1 && menu.price2 && menu.price3 ? (
+                      <div className="flex flex-col w-full">
+                        <i>175ml: &pound; {menu.price1.toFixed(2)}</i>
+                        <i>250ml: &pound; {menu.price2.toFixed(2)}</i>
+                        <i>bottle: &pound; {menu.price3.toFixed(2)}</i>
+                      </div>
+                    ) : (
+                      ''
+                    )}
+                    {!menu.price1 && !menu.price2 && menu.price3 && (
+                      <i>bottle: &pound; {menu.price3.toFixed(2)}</i>
+                    )}
+                  </span>
+                </td>
                 <td>
                   {parse(menu.description)}
-                  <span className="winePrices">
+                  <span className="winePrices max-md:!hidden">
                     {menu.price1 && menu.price2 && menu.price3 ? (
-                      <div className="flex flex-col md:flex-row justify-center w-full md:justify-between">
+                      <div className="flex justify-between w-full">
                         <i>175ml: &pound; {menu.price1.toFixed(2)}</i>
                         <i>250ml: &pound; {menu.price2.toFixed(2)}</i>
                         <i>bottle: &pound; {menu.price3.toFixed(2)}</i>
